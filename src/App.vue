faeb19ddb9868f5dc2958fa9cb78e002e809885b
danilobrinu <ddbn.c2@gmail.com>
2020-01-07 22:34:16 +0100

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="absolute inset-0">
+    <div class="flex relative w-full h-full overflow-hidden">
+      <div class="flex-initial relative w-full max-w-xl h-full shadow-xl z-50">
+        <div class="flex flex-col w-full h-full">
+          <header class="flex-none">
+            <nav class="flex py-4 border-b">
+              <div class="w-full px-6">
+                <div class="flex items-center h-12 shadow-md rounded">
+                  <label for="" class="w-full">
+                    <div class="flex">
+                      <div
+                        class="flex flex-initial w-10 h-10 items-center justify-center cursor-pointer"
+                      >
+                        O
+                      </div>
+                      <input
+                        type="text"
+                        class="flex-auto h-10 font-bold text-gray-900 placeholder-gray-600 rounded"
+                        placeholder="Search a Restaurant"
+                        v-model.trim="query"
+                      />
+                      <div
+                        v-if="query.length"
+                        className="flex flex-initial w-10 h-10 items-center justify-center cursor-pointer"
+                        tabIndex="0"
+                        role="button"
+                        @keydown.prevent.enter.space="query = ''"
+                        @click="query = ''"
+                      >
+                        x
+                      </div>
+                    </div>
+                  </label>
+                </div>
+                <div class="flex mt-3"></div>
+              </div>
+            </nav>
+          </header>
+          <div class="relative flex-1 max-h-full"></div>
+        </div>
+      </div>
+      <div class="flex-auto relative">
+        <div class="absolute inset-0"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from '@vue/composition-api';
+
+window.places = {};
+window.markers = {};
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+    let query = ref('');
+    let setQuery = q => {
+      query = q;
+    };
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    return { query, setQuery };
+  },
+  components: {},
+};
+</script>
